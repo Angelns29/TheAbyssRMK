dc87cae590b75d2a2945f4b783a0518aa869cb85
Angelns29 <angelitonavarretesanchez@gmail.com>
2025-01-29 15:42:59 +0100

    Gravity Change Optimized

--- The Abyss RMK/Assets/CharacterMovement.cs
+++ The Abyss RMK/Assets/CharacterMovement.cs
@@ -14,10 +14,11 @@ public class CharacterMovement : MonoBehaviour
 
     private bool _jumpInput;
     private int _gravity = 4;
-    private bool gravityChanged = false;
+    private bool _gravityChanged = false;
     public GameObject GroundCheck;
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private LayerMask _groundLayer;
+
     void Awake()
     {
         _inputActions = new InputSystem_Actions();
@@ -26,67 +27,60 @@ public class CharacterMovement : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _rb.gravityScale = _gravity;
     }
+
     private void OnEnable()
     {
-        _inputActions.Player.Enable();        
+        _inputActions.Player.Enable();
     }
+
     private void OnDisable()
     {
         _inputActions.Player.Disable();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //Running Input
+        // Running Input
         _moveInput = _inputActions.Player.Move.ReadValue<Vector2>();
         CheckRunning(_moveInput);
-        //Change Gravity
+
+        // Change Gravity
         _inputActions.Player.Jump.performed += i => _jumpInput = true;
         _inputActions.Player.Jump.canceled += i => _jumpInput = false;
-        if (_jumpInput) StartCoroutine(CheckGravity());
 
+        if (_jumpInput && IsGrounded())
+        {
+            ChangeGravity();
+            _jumpInput = false; // Reset jump input to avoid multiple gravity changes
+        }
     }
-
-
 
     private void CheckRunning(Vector2 moveInput)
     {
         if (_moveInput.x != 0)
         {
             _animator.SetBool("isRunning", true);
-            if (_moveInput.x < 0) _spriteRenderer.flipX = true;
-            else _spriteRenderer.flipX = false;
+            _spriteRenderer.flipX = _moveInput.x < 0;
         }
-        else _animator.SetBool("isRunning", false);
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
         _moveInput.y = 0f;
-        _rb.linearVelocity = _moveInput * speed;
+        _rb.linearVelocity = new Vector2(_moveInput.x * speed, _rb.linearVelocity.y);
     }
 
-    IEnumerator CheckGravity()
+    private void ChangeGravity()
     {
-        if (IsGrounded() && gravityChanged == false)
-        {
-            _spriteRenderer.flipY = true;
-            _groundCheck.localPosition = new Vector3(0.32f, 0.85f, 0);
-            _gravity *= -1;
-            _rb.gravityScale = _gravity;
-            gravityChanged = true;
-        }
-        else if (IsGrounded() && gravityChanged == true)
-        {
-            _spriteRenderer.flipY = false;
-            _groundCheck.localPosition = new Vector3(0.32f, -0.95f, 0);
-            _gravity *= -1;
-            _rb.gravityScale = _gravity;
-
-        }
-        yield return new WaitForSeconds(1);
+        _gravityChanged = !_gravityChanged;
+        _spriteRenderer.flipY = _gravityChanged;
+        _groundCheck.localPosition = new Vector3(0.32f, _gravityChanged ? 0.85f : -0.95f, 0);
+        _gravity *= -1;
+        _rb.gravityScale = _gravity;
     }
 
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
-        
     }
 }
